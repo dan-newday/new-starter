67c69c0f8a11f046e9ecc7e6946ee2fa152d2509
Dan Heaver <dan.heaver@newday.co.uk>
2022-02-04 14:55:57 +0000

    debug

--- scripts/installBasePackages.sh
+++ scripts/installBasePackages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # install git and other applications useful for the environment
-sudo apt install -y git make zip unzip net-tools zsh 
+sudo apt install -y git make zip unzip net-tools zsh
--- scripts/installBrew.sh
+++ scripts/installBrew.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 /bin/bash -c "NONINTERACTIVE=true $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+sudo apt install -y build-essential
+
