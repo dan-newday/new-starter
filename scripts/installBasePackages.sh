6e190b2b092f1a757bc61c9009ce7bd85c1052d0
Dan <98327175+dan-newday@users.noreply.github.com>
2022-02-04 13:20:49 +0000

    Update installBasePackages.sh

--- scripts/installBasePackages.sh
+++ scripts/installBasePackages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # install git and other applications useful for the environment
-sudo apt install -y git make zip unzip net-tools zsh sudo build-essential
+sudo apt install -y git make zip unzip net-tools zsh
