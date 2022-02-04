cd33b491a28e4670660b72991174fbff8db6976f
Dan Heaver <dan.heaver@newday.co.uk>
2022-02-04 13:15:25 +0000

    debug

--- scripts/installBrew.sh
+++ scripts/installBrew.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
 /bin/bash -c "NONINTERACTIVE=true $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/newday/.zprofile
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
